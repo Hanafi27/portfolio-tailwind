c8b355310331bebfd549d129b3c71d940359584f
Hanafi Ilham <hanafiilham333@gmail.com>
2024-09-27 15:38:40 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html", "./src/**/*.{html,js}"],
   darkMode: "class",
   theme: {
     extend: {
-      backgroundImage: {
-        "hero-pattern": "url('./img/hans.png')",
-      },
       container: {
         center: true,
         padding: "16px",
